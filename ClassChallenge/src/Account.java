ef9aeb5616752e3017bec6611f7fb5abd7d868ca
weshy <josephwaweru96@gmail.com>
2023-01-01 12:45:02 +0300

    classes

---
+++ ClassChallenge/src/Account.java
@@ -0,0 +1,68 @@
+public class Account {
+        private String accountNumber;
+        private double accountBalance;
+        private String customerName;
+        private String customerEmail;
+        private String phoneNumber;
+
+        public void depositFunds(double depositAmount) {
+            accountBalance += depositAmount;
+            System.out.println("Deposit of KSH " + depositAmount + " made. New " +
+                    "balance is KSH " + accountBalance);
+        }
+
+        public void withdrawFunds(double withdrawalAmount) {
+            if(accountBalance - withdrawalAmount < 0) {
+                System.out.println("Insufficient Funds! You only have KSH " + accountBalance +
+                " in your account.");
+            } else {
+                accountBalance -= withdrawalAmount;
+                System.out.println("Withdrawal of KSH " + withdrawalAmount +
+                        " processed. Remaining balance is KSH " + accountBalance);
+            }
+        }
+
+
+        public double getAccountBalance() {
+            return accountBalance;
+        }
+
+        public String getAccountNumber() {
+            return accountNumber;
+        }
+
+        public void setAccountNumber(String accountNumber) {
+            this.accountNumber = accountNumber;
+        }
+
+        public void setAccountBalance(int accountBalance) {
+            this.accountBalance = accountBalance;
+        }
+
+        public String getCustomerName() {
+            return customerName;
+        }
+
+        public void setCustomerName(String customerName) {
+            this.customerName = customerName;
+        }
+
+        public String getEmail() {
+            return customerEmail;
+        }
+
+        public void setEmail(String email) {
+            this.customerEmail = email;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+
+}
+
---
+++ ClassChallenge/src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+//        Account weshysAccount = new Account();
+//
+//        weshysAccount.setAccountNumber("12345678");
+//        weshysAccount.setAccountBalance(1000);
+//        weshysAccount.setCustomerName("Weshy Joe");
+//        weshysAccount.setEmail("weshyjoe@weshy.com");
+//        weshysAccount.setPhoneNumber("(254)703-536688");
+//
+//        weshysAccount.withdrawFunds(100.0);
+//        weshysAccount.depositFunds(500.0);
+//        weshysAccount.withdrawFunds(2700.0);
+
+//        SimpleCalculator calculator = new SimpleCalculator();
+//        calculator.setFirstNumber(5.0);
+//        calculator.setSecondNumber(4);
+//        System.out.println("add= " + calculator.getAdditionResult());
+//        System.out.println("subtract= " + calculator.getSubtractionResult());
+//        calculator.setFirstNumber(5.25);
+//        calculator.setSecondNumber(0);
+//        System.out.println("multiply= " + calculator.getMultiplicationResult());
+//        System.out.println("divide= " + calculator.getDivisionResult());
+
+        Person person = new Person();
+        person.setFirstName("");// firstName is set to empty string
+        person.setLastName(""); // lastName is set to empty string
+        person.setAge(10);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setFirstName("John"); // firstName is set to John
+        person.setAge(18);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setLastName("Smith"); // lastName is set to Smith
+        System.out.println("fullName= " + person.getFullName());
+
+    }
+}
