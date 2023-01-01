3db048a8e234f1a41e62e817a882bf10358e6df6
weshy <josephwaweru96@gmail.com>
2023-01-01 17:31:39 +0300

    customer constructors

--- ClassChallenge/src/Main.java
+++ ClassChallenge/src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        Account weshysAccount = new Account("12345", 1000.00, "Weshy Joe",
-                "weshyjoes@gmail.com", "(254) 101-236688");
+        Customer weshy = new Customer("Weshy", 1000.0, "weshy@weshy.co");
+
+//        Account weshysAccount = new Account("12345", 1000.00, "Weshy Joe",
+//                "weshyjoes@gmail.com", "(254) 101-236688");
 
 //        Account weshysAccount = new Account();
 
@@ -14,9 +16,9 @@ public class Main {
 //        weshysAccount.setEmail("weshyjoe@weshy.com");
 //        weshysAccount.setPhoneNumber("(254)703-536688");
 
-        weshysAccount.withdrawFunds(100.0);
-        weshysAccount.depositFunds(500.0);
-        weshysAccount.withdrawFunds(2700.0);
+//        weshysAccount.withdrawFunds(100.0);
+//        weshysAccount.depositFunds(500.0);
+//        weshysAccount.withdrawFunds(2700.0);
 
 //        SimpleCalculator calculator = new SimpleCalculator();
 
